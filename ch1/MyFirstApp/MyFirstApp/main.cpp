0556856eba38092218abdee1fae560e1f2c36d7f
Drew Morris <dhmorris@gmail.com>
2015-06-25 08:52:12 -0400

    creating project and gitignore

---
+++ ch1/MyFirstApp/MyFirstApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFirstApp
+//
+//  Created by Drewry Morris on 6/25/15.
+//  Copyright (c) 2015 Drewry Morris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
